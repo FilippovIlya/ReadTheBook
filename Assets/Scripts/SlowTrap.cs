aac0279f2c4c2ea634efe7d7b21d2493fc1d3590
StormSpirta <novumchemia@gmail.com>
2019-10-22 15:29:49 +0300

    delete useless tiles, add slowtrap

---
+++ Assets/Scripts/SlowTrap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowTrap : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.GetComponent<LeftJoystickPlayerController>().moveSpeed = 3f;
+        }
+
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.GetComponent<LeftJoystickPlayerController>().moveSpeed = 5f;
+        }
+    }
+}
